043e847a201b35673c9596adf64418bb9f76c1ee
Peter Phan <peterphan_1996@live.com>
2017-04-30 19:54:20 +0700

    fuck

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@
 #
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
-
+workers Integer(ENV['WEB_CONCURRENCY'] || 0)
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
